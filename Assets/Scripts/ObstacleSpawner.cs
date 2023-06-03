9d1f804faf8a9da5df7a5519ebd3b915bbfa7f98
Hung Le <dark.knight.hp2379@gamil.com>
2023-06-04 01:50:27 +0700

    first commit

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+	[SerializeField] private float waitTime;
+	[SerializeField] private GameObject[] obstaclePrefabs;
+	private float tempTime;
+
+	void Start(){
+		tempTime = waitTime - Time.deltaTime;
+	}
+
+	void LateUpdate () {
+		if(GameManager.Instance.GameState()){
+			tempTime += Time.deltaTime;
+			if(tempTime > waitTime){
+				// Wait for some time, create an obstacle, then set wait time to 0 and start again
+				tempTime = 0;
+				GameObject pipeClone = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position, transform.rotation);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.transform.parent != null){
+			Destroy(col.gameObject.transform.parent.gameObject);
+		}else{
+			Destroy(col.gameObject);
+		}
+	}
+
+}
